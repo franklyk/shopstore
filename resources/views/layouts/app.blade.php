ab391e9090b9b4f6517595486ad8d9096e0a18d3
Franklin da Silva Batista <frsbatist@gmail.com>
2026-04-30 08:05:22 -0300

    Criando cart.js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>{{ env('APP_NAME') }}  @yield('title', 'Home')</title>
+    <title>{{ env('APP_NAME') }} @yield('title', 'Home')</title>
 
     <!-- Bootstrap 5 CDN -->
     {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"> --}}
-
+    <script>
+        window.csrfToken = "{{ csrf_token() }}";
+    </script>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
