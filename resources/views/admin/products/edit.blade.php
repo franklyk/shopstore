36e548547e8104d4a764529a98a08c0ec056dbb1
Franklin da Silva Batista <frsbatist@gmail.com>
2026-06-17 17:09:48 -0300

    layout products.edit

--- resources/views/admin/products/edit.blade.php
+++ resources/views/admin/products/edit.blade.php
@@ -19,8 +19,7 @@
                     @endcan
 
                     @can('edit products')
-                        <x-buttons.button for="edit-form" color="edit" icon="edit"
-                            label="Salvar" />
+                        <x-buttons.button for="edit-form" color="edit" icon="edit" label="Salvar" />
                     @endcan
 
                 </div>
@@ -53,36 +52,31 @@
                     <div class="card-vs">
 
                         <h3 class="section-title">Categorias</h3>
-                        <p class="form-label">
-                            <strong></strong>
-                        </p>
 
-                        <div class="rounded p-3">
-                            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-3">
-                                @forelse($categories as $parent)
-                                    <div class="col">
-                                        <div class="card-vs">
-                                            <div class="fw-bold text-primary mb-2">
-                                                {{ $parent->name }}
-                                            </div>
-                                            <div class="ms-2">
-                                                @forelse($parent->children as $child)
-                                                    <x-admin.forms.checkbox :name="$child->name" :label="$child->name"
-                                                        value="{{ $child->id }}" id="{{ $child->slug }}" />
-                                                @empty
-                                                    <small class="text-muted">Sem subcategorias</small>
-                                                @endforelse
-                                            </div>
-
+                        <div class="auto-grid">
+                            @forelse($categories as $parent)
+                                    <div class="card-vs">
+                                        <div class="section-title text-center">
+                                            {{ $parent->name }}
                                         </div>
+                                        <div class="ms-2">
+                                            @forelse($parent->children as $child)
+                                                <x-admin.forms.checkbox :name="$child->name" :label="$child->name"
+                                                    value="{{ $child->id }}" id="{{ $child->slug }}" />
+                                            @empty
+                                                <small class="text-muted">Sem subcategorias</small>
+                                            @endforelse
+                                        </div>
+
                                     </div>
-                                @empty
-                                    <div class="col">
-                                        <p class="text-muted mb-0">Nenhuma categoria cadastrada.</p>
-                                    </div>
-                                @endforelse
-                            </div>
+
+                            @empty
+                                <div class="col">
+                                    <p class="text-muted mb-0">Nenhuma categoria cadastrada.</p>
+                                </div>
+                            @endforelse
                         </div>
+
                     </div>
                 </x-forms.row>
 
